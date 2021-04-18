b072940fa641271c00e3e3d9698b23cce6d1dec7
Ben Cawrse <bcawrse@gmail.com>
2021-04-18 00:29:24 -0500

    typescript setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
   env: {
+    node: true,
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb', 'prettier', 'plugin:prettier/recommended', 'eslint:recommended', 'plugin:react/recommended'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      'Hyperlink',
+      { name: 'Link', linkAttribute: 'to' },
+    ],
+  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -11,13 +23,25 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: [
+    'react', //
+    'prettier',
+    '@typescript-eslint',
+  ],
+  extends: [
+    'prettier', //
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   rules: {
     // react suppression help https://stackoverflow.com/a/61160875/2957396
     // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
     // allow jsx syntax in js files (for next.js project)
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // should add ".ts" if typescript project
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }], // should add ".ts" if typescript project
     'prettier/prettier': 'error',
   },
 }
