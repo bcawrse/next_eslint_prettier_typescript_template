1badb9578cc6289fdc69f92bd52eb2075d9b6354
Ben Cawrse <bcawrse@gmail.com>
2021-04-17 23:12:54 -0500

    prettier & eslint being friends

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['airbnb', 'prettier', 'plugin:prettier/recommended', 'eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -16,16 +11,13 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
     // react suppression help https://stackoverflow.com/a/61160875/2957396
     // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
     // allow jsx syntax in js files (for next.js project)
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }], // should add ".ts" if typescript project
-    'prettier/prettier': 'error'
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // should add ".ts" if typescript project
+    'prettier/prettier': 'error',
   },
-};
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
